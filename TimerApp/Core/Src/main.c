1975cddc0c07ac0f0fba717dfbd8be528c6cb2c5
lingex <lingex@outlook.com>
2022-07-16 15:55:33 +0800

    unused code

--- TimerApp/Core/Src/main.c
+++ TimerApp/Core/Src/main.c
@@ -103,7 +103,6 @@ _COUNTER runCounter = {
 
 
 extern uint8_t playing;
-extern USBD_HandleTypeDef hUsbDeviceFS;
 
 
 /* USER CODE END PV */
@@ -1048,64 +1047,7 @@ void Sleep(void)
 	{
 		HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
 	}
-#if 1
 	SystemClock_Config();
-	// MX_DAC_Init();
-#else
-	{
-		RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  		RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  		RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
-		
-  		/** Initializes the RCC Oscillators according to the specified parameters
-  		* in the RCC_OscInitTypeDef structure.
-  		*/
-  		RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
-  		RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  		RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
-  		RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  		RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  		RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  		RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  		RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  		if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  		{
-  		  Error_Handler();
-  		}
-		
-  		/** Initializes the CPU, AHB and APB buses clocks
-  		*/
-  		RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-  		                            |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  		RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  		RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  		RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  		RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-		
-  		if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  		{
-  		  Error_Handler();
-  		}
-  		PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC|RCC_PERIPHCLK_ADC
-  		                            |RCC_PERIPHCLK_I2S2|RCC_PERIPHCLK_USB;
-  		PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSI;
-  		PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV8;
-  		PeriphClkInit.I2s2ClockSelection = RCC_I2S2CLKSOURCE_SYSCLK;
-  		PeriphClkInit.UsbClockSelection = RCC_USBCLKSOURCE_PLL_DIV1_5;
-  		if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  		{
-  		  Error_Handler();
-  		}
-	}
-#endif
-#if 0
-	//if (usbDet != 0)
-	{
-		HAL_UART_Init(&huart3);
-		sprintf(tmpstr, "Wake up, 20%02d-%02d-%02d, tick:%lu.\n", rtc.Year, rtc.Month, rtc.Date, HAL_GetTick());
-		HAL_UART_Transmit(&huart3, (uint8_t *)tmpstr, strlen(tmpstr), 100);
-	}
-#endif
 
 	//{
 	// disable BTN interrupt
